16f4c34b05f50bde4b7f16d5473e11ec0bbcf70b
James-Ashley <jkbashley@gmail.com>
2020-12-21 19:38:38 -0500

    changes were made

--- app.py
+++ app.py
@@ -1,10 +1,59 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+#POSTGRESS DEPENDENCY
+from flask_sqlalchemy import SQLAlchemy
+
+from os import environ
+
+from flask_pymongo import PyMongo
+
+import json
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL', 'sqlite:///notepad.sqlite')
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.integer, primary_key=True)
+    description = db.Column(db.String)
+
+
+app.config['MONGO_URI'] = environ.get('MONGODB_URI') or 'mongodb://localhost:27071/heroku-notepad'
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
     return render_template('index.html', name="James")
+# POSTGRES
+@app.route('/api/tasks-postgres')
+def getTasksPostgres():
+    tasks = db.session.query(Task)
+    data = []
+
+    for task in tasks:
+        item = {
+            'id': task.id,
+            'description': task.description
+        }
+        data.append(item)
+    
+    return jsonify(data)
+
+@app.route('/tasks')
+def tasks():
+    tasks = mongo.db.tasks.find({})
+    data = []
+    
+    for task in tasks:
+        item = {
+            "_id": str(task['_id']),
+            "description": task['description']
+        }
+        data.append(item)
+
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
